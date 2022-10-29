3954035d86f6fde60355c8ee13361ba764753636
druharo <111248621+druharo@users.noreply.github.com>
2022-10-29 17:59:55 +0300

    populated questions array

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Project Title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Table of Contents?',
+        name: 'toc',
+    },
+    {
+        type: 'input',
+        message: 'Installation?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'License?',
+        name: 'license',
+        choices: ["MIT", "GPL", "GNU", "BSD", "APL"]
+    },
+    {
+        type: 'input',
+        message: 'Contributing Guidelines?',
+        name: 'contributions',
+    },
+    {
+        type: 'input',
+        message: 'Test Instructions?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Questions?',
+        name: 'questions',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
@@ -9,10 +56,11 @@ function writeToFile(fileName, data) { }
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(
-            questions
-        )
+        questions
+    )
         .then((answers) => {
             // Use user feedback for... whatever!!
+            writeToFile("README.md", answers)
         })
         .catch((error) => {
             if (error.isTtyError) {
